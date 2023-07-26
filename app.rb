c7617eb74bea77bc96119d1f874454b7a16bc37b
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-07-26 20:27:47 +0000

    Write old umbrella code

--- app.rb
+++ app.rb
@@ -9,3 +9,26 @@ end
 get("/umbrella"){
   erb(:umbrella)
 }
+
+post("/process_umbrella"){
+
+  @location = params.fetch("location")
+
+  GMAPS_URI = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@location}&key=#{ENV.fetch("GMAPS_KEY")}"
+
+  gmaps_req = HTTP.get(GMAPS_URI)
+
+  gmaps_res = JSON.parse(gmaps_req)
+
+  lat = gmaps_res.fetch("results").at(0).fetch("geometry").fetch("location").fetch("lat")
+  lng = gmaps_res.fetch("results").at(0).fetch("geometry").fetch("location").fetch("lng")
+
+  PIRATE_URI = "https://api.pirateweather.net/forecast/#{ENV.fetch("PIRATE_WEATHER_KEY")}/#{lat},#{lng}"
+
+  pirate_req = HTTP.get(PIRATE_URI) 
+  pirate_res = JSON.parse(pirate_req) 
+
+  @output = pirate_res
+
+  erb(:umbrella_output)
+}
